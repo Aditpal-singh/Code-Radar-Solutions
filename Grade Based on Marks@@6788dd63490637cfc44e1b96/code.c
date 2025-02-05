1cd2879dac902cf993aa49be267bdfa8e2a6981b
Aditpal-singh <aditpal0897.becse24@chitkara.edu.in>
2025-02-05 16:51:46 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() 
+{   if (x>=90)
+    {
+        printf("A");
+    }
+    else if(x>=80 && x<90)
+    {
+        printf("B");
+    }
+    else if(x>=70 && x<80)
+    {
+        printf("C");
+    }
+    else if(x>=60 && x<70)
+    {
+        printf("D");
+    }
+    else if(x<60)
+    {
+        printf("F");
+    }
+
+    return 0;
+}
