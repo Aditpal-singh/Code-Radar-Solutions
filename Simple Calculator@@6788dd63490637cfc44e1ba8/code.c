aaddb6ce1f6e9ce228bef95c530eafa705703e73
Aditpal-singh <aditpal0897.becse24@chitkara.edu.in>
2025-02-18 11:25:13 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
-int main() 
-{   
-    int x, y,r;
-    char z;
-    
-    scanf("%d %d", &x, &y);
-    scanf("%c", &z);  // Read operator
-      // Read two integers
 
-    if (z == '+') {
-        printf("%d + %d = %d", x, y, x + y);
-    }
-    else if (z == '-') {
-        r = x - y;
-        printf("%d",r);
-    }
-    else if (z == '*') {
-        printf("%d * %d = %d", x, y, x * y);
-    }
+int main() {
+    double num1, num2, result;
+    char op;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
     
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &op); // Note the space before %c to consume newline
+
+   
+
+    // Performing operations using if-else
+    if (op == '+') {
+        result = num1 + num2;
+        printf("Result: %d", result);
+    } 
+    else if (op == '-') {
+        result = num1 - num2;
+        printf("Result: %d", result);
+    } 
+    else if (op == '*') {
+        result = num1 * num2;
+        printf("Result: %d", result);
+    } 
+    else if (op == '/') {
+        if (num2 != 0) { // Prevent division by zero
+            result = num1 / num2;
+            printf("%d", result);
+        } else {
+            printf("Error: Division by zero is not allowed.\n");
+        }
+    } 
+    else {
+        printf("Invalid operator! Please use +, -, *, or /.\n");
+    }
+
     return 0;
 }
